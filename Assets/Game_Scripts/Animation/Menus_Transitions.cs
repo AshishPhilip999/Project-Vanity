dbeec4e54e802d7cfd0f876547221d1da545399a
AshishPhilip999 <ashishphilip186@gmail.com>
2023-02-26 15:38:12 +0530

    Transition Animation to Menus

---
+++ Assets/Game_Scripts/Animation/Menus_Transitions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menus_Transitions : MonoBehaviour
+{
+
+    public void Menu_ActiveAnimation(GameObject NextMenu)
+    {
+        NextMenu.SetActive(true);
+        NextMenu.GetComponent<Animator>().SetBool("Active" , true);
+    }
+    public void Menu_InactiveAnimation(GameObject CurrentButton)
+    {
+        CurrentButton.transform.parent.GetComponent<Animator>().SetBool("Active", false);
+    }
+
+
+    public void Menu_SetActive()
+    {
+        this.gameObject.SetActive(true);
+    }
+    public void Menu_SetInactive()
+    {
+        this.gameObject.SetActive(false);
+    }
+    
+}
