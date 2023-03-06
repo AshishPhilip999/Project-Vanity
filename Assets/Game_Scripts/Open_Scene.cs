1eeca117900dc8ac72dbcd001642311fc10d3c48
AshishPhilip999 <ashishphilip186@gmail.com>
2023-03-06 23:49:28 +0530

    Added Streamer mode to UI

---
+++ Assets/Game_Scripts/Open_Scene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+public class Open_Scene : MonoBehaviourPunCallbacks
+{
+    public void CreateStreamerMode()
+    {
+        PhotonNetwork.CreateRoom("The_room");
+    }
+
+    public void JoinStreamMode()
+    {
+        PhotonNetwork.JoinRoom("The_room");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Streamer_Mode");
+    }
+}
---
+++ Assets/Game_Scripts/Streamer_Mode/Web_Cam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Web_Cam : MonoBehaviour
+{
+
+    public RawImage image;
+    WebCamTexture webCam;
+
+     void Start()
+    {
+       webCam = new WebCamTexture();
+
+        image.texture = webCam;
+
+        webCam.Play();
+    }
+
+    private void OnDestroy()
+    {
+        webCam.Stop();
+    }
+}
