424de54470745e5a0391f6fec0d11f7321cdd62e
SuryaXHunter <gunturisuryateja@gmail.com>
2023-03-07 15:01:49 +0530

    Changes in streamer mode

--- Assets/Game_Scripts/Server Connection/Host_Join_Connection.cs
+++ Assets/Game_Scripts/Server Connection/Host_Join_Connection.cs
@@ -10,19 +10,6 @@ public class Host_Join_Connection : MonoBehaviourPunCallbacks
     public InputField Host_Room;
     public InputField Join_Room;
 
-    public void CreateMansion()
-    {
-        PhotonNetwork.CreateRoom("The_room");
-    }
 
-    public void JoinMansion()
-    {
-        PhotonNetwork.JoinRoom("The_room");
-    }
-
-    public override void OnJoinedRoom()
-    {
-        PhotonNetwork.LoadLevel("The_Mansion");
-    }
 
 }
--- Assets/Game_Scripts/Streamer_Mode/Stream_Cam_To_Server.cs
+++ Assets/Game_Scripts/Streamer_Mode/Stream_Cam_To_Server.cs
@@ -1,18 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 
-public class Stream_Cam_To_Server : MonoBehaviour
+public class Stream_Cam_To_Server : MonoBehaviourPunCallbacks
 {
+    public RawImage CamImage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+     
     }
 }
